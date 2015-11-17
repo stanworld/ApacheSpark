3c4e9de2df62979f00b9475f92a32c6299f4f098
stan <stan@stan-PC.(none)>
2015-11-16 21:37:41 -0500

    pre final.

--- MLwithSpark/C4/recommendation.py
+++ MLwithSpark/C4/recommendation.py
@@ -6,6 +6,8 @@ from numpy import array
 from pyspark.mllib.recommendation import Rating
 from numpy import linalg as LA
 import numpy as np
+from operator import add
+import math
 
 sc = SparkContext("local[2]", "Recommendation")
 
@@ -69,5 +71,106 @@ sortedItems = sc.parallelize(sortedItems)
 sortedItems2=sortedItems.map(lambda x: (titles[x[0]],x[1]))
 
 sortedItems2.collect()
+
+# mean squared error
+
+actualRating=moviesForUser.take(1)[0][1]
+
+predictedRating= model.predict(789, actualRating.product)
+
+squaredError = pow(predictedRating-actualRating.rating,2.0)
+
+usersProducts= ratings.map(lambda x: (x.user,x.product))
+
+predictions=model.predictAll(usersProducts).map( lambda x: ((x.user,x.product),x.rating))
+
+ratingsAndPredictions = ratings.map( lambda x: ((int(x.user),int(x.product)),float(x.rating))).join(predictions)
+
+MSE=ratingsAndPredictions.map(lambda ((x,y),(z,t)) : pow(z-t,2)).reduce(add)/ratingsAndPredictions.count()
+
+print ("Mean Squared Error %s\n" %MSE)
+
+RMSE = math.sqrt(MSE);
+
+print ("Rooted Mean Squared Error %s\n" %RMSE)
+
+# Mean average precision at K for user 789
+
+actualMovies=moviesForUser.map(lambda x: int(x[1].product))
+predictedMovies = topKRecs.map(lambda x: x.product)
+
+actual=actualMovies.collect()
+predicted = predictedMovies.collect()
+
+def avgPrecisionK( actual, predicted, k):
+    score=0.0
+    numHit=0
+    predKr = np.array(predicted[0:k])
+
+    predK =[]
+    for index,x in np.ndenumerate(predKr):
+       predK.append((x,index[0]))
+
+    for (a,b) in predK:
+        if a in actual:
+            numHit=numHit+1
+            score = score+ numHit/(float(b)+1.0)
+
+    if not actual:
+        return 1.0
+    else:
+        score=score/float(min(len(actual),k))
+
+    return score
+
+# copy functions to IPYTHON
+#1. Copy the lines you want to copy into IPython into the clipboard
+#2. Enter %paste into IPython
+#3. Press enter
+apk10=avgPrecisionK(actual,predicted,10)
+print ("Average precision in 10:")
+f(apk10)
+
+# average precision for all users
+
+itemFactors = model.productFeatures().map( lambda (id,factor): factor).collect()
+
+itemMatrix = np.matrix(itemFactors)
+print("Item factor matrix:")
+f(itemMatrix.shape)
+
+imBroadcast=sc.broadcast(itemMatrix)
+
+def multivector (userId, array0):
+    userVector= np.array(array0)
+    scores = imBroadcast.value.dot(userVector)
+    scores = np.array(scores)[0]
+    sortedWithId =[]
+    for index,x in np.ndenumerate(scores):
+       sortedWithId.append((x,index[0]))
+
+
+    sortedWithId.sort(key=lambda tup: tup[0], reverse=True)
+    recommendationIds=map(lambda x: (x[0],x[1]+1),sortedWithId)
+    return (userId, recommendationIds)
+
+allRecs = model.userFeatures().map( lambda x: multivector(x[0],x[1]))
+
+userMovies = ratings.map(lambda x: (int(x.user),x.product)).groupBy(lambda x: x[0])
+userMovies = userMovies.map(lambda x: (x[0], list(x[1])))
+
+K=10
+
+def fx(predicted, actualWithIds):
+    actual = map(lambda x: int(x[1]),actualWithIds)
+    return avgPrecisionK(actual,predicted, K)
+
+MAPK=allRecs.join(userMovies).map(lambda (userId, (predicted,actualWithIds)): fx(predicted, actualWithIds)).reduce(add)/allRecs.count()
+
+print("Mean Average Precision at K=10 %s" %MAPK)
+
+## use evalution metrics within mllib
+
+
 
 print("Exercise done!\n")
