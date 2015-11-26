d90d5352b7247d6e50e735729c51c6cc9de3a789
stan <stan@stan-PC.(none)>
2015-11-25 21:56:04 -0500

    producer

--- MLwithSpark/C10_SparkStream/SparkStream.py
+++ MLwithSpark/C10_SparkStream/SparkStream.py
@@ -1,1 +1,11 @@
 __author__ = 'stan'
+
+import socket
+import cPickle as pickle
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+address = ('localhost', 9999)
+client.connect(address)
+data=client.recv(1000)
+client.close();
+events=pickle.loads(data)
+print events
---
+++ MLwithSpark/C10_SparkStream/StreamingProducer.py
@@ -0,0 +1,68 @@
+__author__ = 'stan'
+
+import csv
+import random
+import time
+import SocketServer
+import socket
+import threading
+import cPickle as pickle
+with open('names.csv','rt') as fin:
+        cin=csv.reader(fin)
+        names= [row for row in cin]
+
+
+
+products = [
+        ("iPhone cover",9.99),
+        ("Headphones", 5.49),
+        ("Samsung Galaxy Cover",8.95),
+        ("iPad Cover",7.49)
+    ]
+
+
+def oneEvent (i):
+         index=random.randint(0,3)
+         product=products[index][0]
+         price =products[index][1]
+         random.shuffle(names[0]);
+         user=names[0][0];
+         return (user,product,price)
+
+def generateProductEvents ( n ):
+        events=map(lambda i: oneEvent(i),range(0,n))
+        return events
+
+
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+
+    def handle(self):
+            cur_thread = threading.current_thread()
+            print("Got client connected from: %s" %cur_thread.name)
+#        while 1:
+            time.sleep(1)
+            events=generateProductEvents(random.randint(5,10))
+#            data = self.request.recv(1024)
+            print("events sent after serialization:\n%s" %events)
+            data = pickle.dumps(events)
+            self.request.sendall(data)
+
+
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+
+
+if __name__ == "__main__":
+
+
+    HOST, PORT = "localhost", 9999
+
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    ip, port = server.server_address
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    print ("Socket server started at ip %s with port %s\n" % (ip,port) )
+    server.serve_forever()
