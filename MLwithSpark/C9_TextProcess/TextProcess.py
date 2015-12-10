c52385962527498c2b46fb3e45aee102b22590f2
stan <stan@stan-PC.(none)>
2015-12-10 16:08:03 -0500

    scripts.

--- MLwithSpark/C9_TextProcess/TextProcess.py
+++ MLwithSpark/C9_TextProcess/TextProcess.py
@@ -1,1 +1,11 @@
 __author__ = 'stan'
+# shell commands in pyspark
+path = "/home/stan/spark_code/MLwithSpark/C9_TextProcess/20news-bydate-train/*"
+
+
+rdd = sc.wholeTextFiles(path)
+
+text = rdd.map(lambda (file,text): text)
+
+print(text.count())
+
