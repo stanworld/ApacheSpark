7518d92652849ab3a4b76a356ef127b1a23fd310
stan <stan@stan-PC.(none)>
2015-12-14 16:36:54 -0500

    tokenizing and clearning words

--- MLwithSpark/C9_TextProcess/TextProcess.py
+++ MLwithSpark/C9_TextProcess/TextProcess.py
@@ -20,3 +20,40 @@ newsgroups=rdd.map(lambda (file,text): file.split("/")[-2])
 countByGroup = newsgroups.map(lambda n: (n,1)).reduceByKey(add).sortBy(lambda (a,b): b, ascending=False).collect()
 
 print(countByGroup)
+
+##tokenization
+
+whiteSpaceSplit = text.flatMap(lambda line: map(lambda x: x.lower(),line.split(" ")))
+
+print(whiteSpaceSplit.distinct().count())
+
+
+print(','.join(whiteSpaceSplit.sample(True,0,3,42).take(100)))
+
+
+#####splitting each document on nonword characters using regular expression
+
+import re
+
+nonWordSplit=text.flatMap(lambda line: map(lambda x: x.lower(),re.split('\W+',line)))
+print(nonWordSplit.distinct().count())
+
+
+pattern='\w*\d\w*'
+
+filterNumbers=nonWordSplit.filter(lambda token: re.search(pattern,token) is None)
+
+print(filterNumbers.distinct().count())
+
+print(",".join(filterNumbers.distinct().sample(True,0.3,42).take(10)))
+
+
+#####Removing stop words
+
+
+
+
+
+
+
+
