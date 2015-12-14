69128fab777dd27d76e6cb1209638ee140ed73a0
stan <stan@stan-PC.(none)>
2015-12-14 10:39:31 -0500

    data processing

--- MLwithSpark/C9_TextProcess/TextProcess.py
+++ MLwithSpark/C9_TextProcess/TextProcess.py
@@ -1,5 +1,10 @@
 __author__ = 'stan'
 # shell commands in pyspark
+
+# data source: http://qwone.com/~jason/20Newsgroups/20news-bydate.tar.gz
+
+from operator import add
+
 path = "/home/stan/spark_code/MLwithSpark/C9_TextProcess/20news-bydate-train/*"
 
 
@@ -7,5 +12,11 @@ rdd = sc.wholeTextFiles(path)
 
 text = rdd.map(lambda (file,text): text)
 
+
 print(text.count())
 
+newsgroups=rdd.map(lambda (file,text): file.split("/")[-2])
+
+countByGroup = newsgroups.map(lambda n: (n,1)).reduceByKey(add).sortBy(lambda (a,b): b, ascending=False).collect()
+
+print(countByGroup)
